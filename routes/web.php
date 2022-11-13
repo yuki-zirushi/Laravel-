ae251ad613fd8b299e3777fa90e706cd4a59c7eb
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-13 22:43:12 +0900

    [Add route] /building/{room}

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/', function () {
 Route::get('/building', function() {
     return '建物です';
 });
+
+Route::get('/building/{room}', function($room) {
+    return "部屋番号は" . $room . "です";
+});
